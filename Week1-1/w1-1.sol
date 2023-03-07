f928297a78d5b430e15116d9ca9744ca0830054a
PtahSeven <126786904+PtahSeven@users.noreply.github.com>
2023-03-07 22:37:36 +0800

    Add files via upload

---
+++ Week1-1/w1-1.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract w1_1 {
+
+    function add(uint256 x) public pure returns(uint256) {
+        return 1 + x;
+    }
+
+}
